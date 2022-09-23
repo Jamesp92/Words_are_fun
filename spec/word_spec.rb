4853890f9430d778c93ded58d59112ea23073cb1
James Perry <PjPerry28@gmail.com>
2022-09-23 11:49:40 -0700

    write and fail test to save a word in a hash

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,4 +11,14 @@ describe '#Word' do
       expect(Word.all).to(eq([]))
     end
   end
+  
+  describe('#save') do 
+    it('saves a word') do
+      word1 = Word.new("Apple", nil)
+      word1.save()
+      word2 = Word.new("Kiwi", nil)
+      word2.save()
+      expect(Word.all).to(eq([word2, word1]))
+    end
+  end
 end
