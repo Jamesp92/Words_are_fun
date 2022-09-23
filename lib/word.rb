4eda1a2cb5321fd0b82388d5f96e13e27b36a3d1
James Perry <PjPerry28@gmail.com>
2022-09-23 10:38:51 -0700

    initiail commit

---
+++ lib/word.rb
@@ -0,0 +1,13 @@
+class Word
+  attr_reader :id, :word
+  attr_accessor :word 
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(word, id)
+    @word = word
+    @id = id || @@total_rows += 1
+  end
+
+  def seld.all
+    @@words.values().sort { |a, b| a.word.downcase <=> b.word.downcase}
