32a0bc00c044920a162b6d8074316d64ac643988
James Perry <PjPerry28@gmail.com>
2022-10-03 15:56:07 -0700

    pass all integration test

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,3 +12,70 @@ describe('create a word path', {:type => :feature}) do
     expect(page).to have_content('this is a word')
   end
 end
+
+describe('update a word path', {:type => :feature}) do
+  it('updates a word and then goes to the word page') do
+    word = Word.new(word: 'this is a word', id: nil)
+    word.save
+    visit("/word/#{word.id}")
+    click_on('edit word')
+    fill_in('word', :with => 'this is a definition')
+    click_on('Update')
+    expect(page).to have_content('this is a definition')
+  end
+end
+
+describe('delete a word path', {:type => :feature}) do
+  it('deletes a word and then goes to the word page') do
+    Word.clear
+    word = Word.new(word: 'this is a word', id: nil)
+    word.save
+    visit("/word/#{word.id}")
+    click_on('edit word')
+    click_on('Delete')
+    expect(page).to have_no_content('this is a word')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a definition and then goes to the word page') do
+    Word.clear
+    word = Word.new(word: 'this is a word', id: nil)
+    word.save
+    visit("/word/#{word.id}")
+    fill_in('word_definition', :with => 'this is a definition')
+    click_on('Add definition')
+    expect(page).to have_content('this is a definition')
+  end
+end
+
+describe('delete a definition path', {:type => :feature}) do
+  it('deletes a definition and then goes to the word page') do
+    Word.clear
+    Definition.clear
+    word = Word.new(word: 'this is a word', id: nil)
+    word.save
+    definition = Definition.new(definition: 'this is a definition', id: nil, w_id: word.id)
+    definition.save
+    visit("/word/#{word.id}")
+    click_on('this is a definition')
+    click_on('Delete definition')
+    expect(page).to have_no_content('this is a definition')
+  end
+end
+
+describe('update a definition path', {:type => :feature}) do
+  it('updates a definintion and then goes to the word page') do
+    Word.clear
+    Definition.clear
+    word = Word.new(word: 'this is a word', id: nil)
+    word.save
+    definition = Definition.new(definition: 'this is a definition', id: nil, w_id: word.id)
+    definition.save
+    visit("/word/#{word.id}")
+    click_on('this is a definition')
+    fill_in('definition_edit', :with => 'this is a new definition')
+    click_on('Update')
+    expect(page).to have_content('this is a new definition')
+  end
+end
